c72b2c12b3d589c1b948396736cac22cf7aeba38
Nitsan Wakart <nitsanw@yahoo.com>
2013-03-02 20:10:17 +0000

    interface and 2 implementation

--- src/com/pls/mergeq/LinkedMergingQueue.java
+++ src/com/pls/mergeq/LinkedMergingQueue.java
@@ -4,16 +4,18 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 
 /**
- * A merging queue for values with keys. Null key/values are not welcome.
+ * A merging queue for values with keys. Null key/values are not welcome. Implemented
+ * using a LinkedHashMap.
  * 
  * @author nitsanw
  *
  * @param <KEY>
  * @param <VAL>
  */
-public class LinkedMergingQueue<KEY, VAL> {
-	private final LinkedHashMap<KEY, VAL> lastValMap = new LinkedHashMap<KEY, VAL>();
+public class LinkedMergingQueue<KEY, VAL>  implements MergingQueue<KEY, VAL>{
+	private final LinkedHashMap<KEY, VAL> lastValMap = new LinkedHashMap<KEY, VAL>(1024);
 
+	@Override
 	public VAL poll() {
 		if (lastValMap.isEmpty())
 			return null;
@@ -21,11 +23,19 @@ public class LinkedMergingQueue<KEY, VAL> {
 		return lastValMap.remove(iterator.next());
 	}
 	
+	@Override
 	public void offer(KEY key, VAL val) {
 		assert key != null && val != null;
 		lastValMap.put(key, val);
 	}
+	
+	@Override
 	public boolean isEmpty() {
 		return lastValMap.isEmpty();
+	}
+	
+	@Override
+	public void clear() {
+	    lastValMap.clear();
 	}
 }
